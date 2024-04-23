db9b4579fcbbbc02a574e420d86897360fa69195
isqijing <junfenghe.cloud@qq.com>
2024-04-23 08:55:04 +0800

    fix and support blob

---
+++ a_test/blob_test.go
@@ -0,0 +1,81 @@
+package a_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"proxy.cloudwave.cn/share/go-sql-driver/cloudwave"
+	"proxy.cloudwave.cn/share/go-sql-driver/cloudwave/a_test/junfenghe/common"
+	"testing"
+)
+
+var mapName = struct {
+	schema string
+}{"itest_go"}
+
+// tearup 代码
+func TestMain(m *testing.M) {
+	// 初始化数据库连接
+	db, err := common.InitCloudwaveFull(common.ReqInitCloudwaveFull{Schema: ""})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	exec, err := db.Exec("drop schema if exists " + mapName.schema)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("tearup ...")
+	log.Println(exec)
+	result, err := db.Exec("create schema " + mapName.schema)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(result)
+	// ...
+
+	// 运行测试
+	code := m.Run()
+
+	// 清理数据库连接
+	// ...
+
+	// 退出测试
+	os.Exit(code)
+}
+
+func TestBlob(t *testing.T) {
+
+	db, err := common.InitCloudwaveFull(common.ReqInitCloudwaveFull{Schema: mapName.schema})
+
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = db.Exec(fmt.Sprintf("create table %s(id integer,c1 blob, primary key(id))", t.Name()))
+	if err != nil {
+		t.Error(err)
+	}
+	prepare, err := db.Prepare("insert into " + t.Name() + " values(?, ?)")
+	if err != nil {
+		t.Error(err)
+	}
+	exec, err := prepare.Exec(1, []byte("1234567890"))
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(exec)
+
+	rows, err := db.Query("select * from " + t.Name())
+	for rows.Next() {
+		var id int
+		//var c1 []byte
+		var c1 *cloudwave.CloudBlob
+		rows.Scan(&id, &c1)
+
+		bytes, err := c1.GetBytes()
+		if err != nil {
+			t.Error(err)
+		}
+		log.Println(id, string(bytes))
+	}
+
+}
